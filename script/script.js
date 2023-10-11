370e696600d29cdc9ec12023d3529f5c1b18ac9a
akademuk <akademuk24@gmail.com>
2023-10-11 14:48:24 +0300

    sss

--- script/script.js
+++ script/script.js
@@ -35,3 +35,42 @@ const partners = new Swiper('.partners-slider', {
 
     loop: false, // Отключение бесконечной петли
 });
+
+
+
+// Аккардион
+$(document).ready(() => {
+    // Hide all accordion contents when the page loads
+    $(".accardion-contents").hide();
+
+    // Add "active" class to the first .accardion-heading element
+    $(".accardion-heading:first").addClass("active");
+
+    // Show the content of the first accordion section
+    $(".accardion-heading:first")
+        .next(".accardion-contents")
+        .slideDown();
+
+    $(".accardions").on("click", ".accardion-heading", function () {
+        const $accordionContainer = $(this).closest(".accordion-container");
+
+        // Check if the current heading has the "active" class
+        const isActive = $(this).hasClass("active");
+
+        // Hide all accordion contents
+        $(".accardion-contents").slideUp(300);
+
+        // Remove the "active" class from all headings
+        $(".accardion-heading").removeClass("active");
+
+        // Remove the "active" class from all accordion containers
+        $(".accordion-container").removeClass("active");
+
+        if (!isActive) {
+            // Show the content and add the "active" class only to the current heading and its accordion-container
+            $(this).next().slideDown();
+            $accordionContainer.addClass("active");
+            $(this).addClass("active");
+        }
+    });
+});
